ae5a089435c2d8b40b29f76a009f44e266054dd5
Adam Denyer <95426758+AdamDenyer@users.noreply.github.com>
2025-12-16 17:26:24 +0000

    C#: Add execution times and a header to the console output

--- CSharp/App/Day.cs
+++ CSharp/App/Day.cs
@@ -2,11 +2,15 @@ namespace AdventOfCode;
 
 public abstract class Day{
     public virtual void Main(int year, int day, bool useExamples){
+        var timer = System.Diagnostics.Stopwatch.StartNew();
         long part1 = Part1(Inputs.BuildFilepath(year, day, useExamples));
-        Console.WriteLine($"Day {day}-1: {part1}");
+        timer.Stop();
+        Console.WriteLine($"Day {day}-1: {part1} ({timer.ElapsedMilliseconds}ms)");
 
+        timer = System.Diagnostics.Stopwatch.StartNew();
         long part2 = Part2(Inputs.BuildFilepath(year, day, useExamples));
-        Console.WriteLine($"Day {day}-2: {part2}");
+        timer.Stop();
+        Console.WriteLine($"Day {day}-2: {part2} ({timer.ElapsedMilliseconds}ms)");
     }
     //Enforce existence of Part1() and Part2() in child classes so Main() can always call them:
     public abstract long Part1(string filepath);
--- CSharp/App/Program.cs
+++ CSharp/App/Program.cs
@@ -11,7 +11,10 @@ List<Day> days = Settings.CurrentYearString switch{
     _ => new Year2021().Days() //Default outcome if no value matches
 };
 
+Console.WriteLine($"---------------------");
+Console.WriteLine($" Advent of Code {year} ");
+Console.WriteLine($"---------------------");
 for (int day = 1; day <= today; day++){
     if (runAllDays == false) {day = today;}
     days[day-1].Main(year, day, useExamples);
-    }
+}
